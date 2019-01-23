3bd9dd9af2db896a4b67d206d8efe8209b18fc0a
sbx_user1060 <sbx_user1060@ip-10-128-93-206.eu-west-2.compute.internal>
2019-01-23 15:36:06 +0000

    Wed Jan 23 15:36:06 UTC 2019

--- src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -56,9 +56,7 @@ namespace BeFaster.App.Solutions.FIZ
                     if (intThree) rtnValue = "fizz";
                     if (intFive) rtnValue = "buzz";
                     if (sameVal) rtnValue = deluxeValue;
-                    if (intThree && sameVal && i.ToString().Contains("3"))
-                        rtnValue = "fizz" ;
-                    else if (intThree && sameVal) rtnValue = "fizz " + deluxeValue;
+                    if (intThree && sameVal) rtnValue = "fizz " + deluxeValue;
                     if (intFive && sameVal) rtnValue = "buzz " + deluxeValue;
                     if (intThree && intFive) rtnValue = "fizz buzz";
                     if (intThree && intFive && sameVal) rtnValue = "fizz buzz " + deluxeValue;
@@ -69,3 +67,4 @@ namespace BeFaster.App.Solutions.FIZ
         }
     }
 }
+
