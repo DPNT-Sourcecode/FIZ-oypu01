1f949577ed9bc82924143ab9a4efd065f705c3b7
sbx_user1060 <sbx_user1060@ip-10-128-93-206.eu-west-2.compute.internal>
2019-01-23 14:23:02 +0000

    Wed Jan 23 14:23:02 UTC 2019

--- src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -7,7 +7,7 @@ namespace BeFaster.App.Solutions.FIZ
         public static string FizzBuzz(int number)
         {
             string rtnValue = string.Empty;
-            for(int i = 0; i < number; ++i )
+            for(int i = 1; i < number; i++ )
             {
                 bool intThree = (i % 3 == 0 ? true : false) || (i.ToString().Contains("3") ? true : false);
                 bool intFive = (i % 5 == 0 ? true : false) || (i.ToString().Contains("5") ? true : false);
@@ -26,4 +26,5 @@ namespace BeFaster.App.Solutions.FIZ
         }
     }
 }
+
 
